f2f82e04eede113376023fc4a2fd95cf580dae60
HAnh3112 <haianh311205@gmail.com>
2025-12-07 07:17:54 +0700

    Init mfc

---
+++ Wlpm/Wlpm/Wlpm.h
@@ -0,0 +1,32 @@
+﻿
+// Wlpm.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CWlpmApp:
+// See Wlpm.cpp for the implementation of this class
+//
+
+class CWlpmApp : public CWinApp
+{
+public:
+	CWlpmApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CWlpmApp theApp;
